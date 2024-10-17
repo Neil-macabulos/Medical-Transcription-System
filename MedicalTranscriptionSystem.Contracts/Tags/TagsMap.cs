389930e04c3d88757f44f34e24cbbd087ee58bf6
Neil Macabulos <nmacabulos@csmbataan.ph.education>
2024-10-17 12:13:22 +0800

    Prelim & Pre Midterm

---
+++ MedicalTranscriptionSystem.Contracts/Tags/TagsMap.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MedicalTranscriptionSystem.Contracts.Users;
+using MedicalTranscriptionSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalTranscriptionSystem.Contracts.Tags
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<Tags, TagsDto>();
+            CreateMap<TagsDto, Tags>();
+        }
+    }
+}
---
+++ MedicalTranscriptionSystem.Contracts/Users/UsersMap.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MedicalTranscriptionSystem.Contracts.Users;
+using MedicalTranscriptionSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalTranscriptionSystem.Contracts.Users
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<Users, UsersDto>();
+            CreateMap<UsersDto, Users>();
+        }
+    }
+}
